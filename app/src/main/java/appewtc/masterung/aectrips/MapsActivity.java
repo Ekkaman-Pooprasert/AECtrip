f965625bafa697d3584621aa8972b3eb292bb4e1
test1 <abc@gmail.com>
2015-12-08 10:21:36 +0700

    Marker Title Finish

--- app/src/main/java/appewtc/masterung/aectrips/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/aectrips/MapsActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.aectrips;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -115,10 +117,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             title(titleStrings[i]));
         }
 
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(latCenterADouble, lngCenterADouble);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Long Click on Marker
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                Intent objIntent = new Intent(MapsActivity.this, MarkerDetailActivity.class);
+                String strTitle = marker.getTitle();
+                objIntent.putExtra("Title", strTitle);
+                startActivity(objIntent);
+
+                return true;
+            }
+        });
+
 
     }   // onMapsReady
 }   //Main Class
